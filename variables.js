6d399db17d79c7d01713a9a5943ba3f5038d534b
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-08 05:43:56 -0800

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,19 @@
+const studentId=2201297283
+let studentEmail="soumya@56743gmail.com"
+var studentPassword="ranjan@7678"
+studentCity="Bhubaneswar"
+let studentState;
+
+//studentId=67897898 // not allowed
+
+console.log(studentId)
+console.log(studentEmail)
+console.log(studentPassword)
+console.log(studentCity)
+
+/*
+Prefer not to use var
+because of issue in block scope and funtional scope
+*/
+
+console.table([studentId,studentEmail,studentPassword,studentCity]) // if you want to print the data in table format and index format
