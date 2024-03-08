2d97aa6fbd44b8f2b0d120d67f30ab285ce55792
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-08 11:06:02 -0800

    Add files via upload

---
+++ Data_type.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using nodejs,not broweser
+
+//number => 2 to the power 53 
+//bigint
+//string =>" Hello"
+//boolean=> true/false
+//null => standlone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // it give output as object
+console.log(typeof undefined); // it give output as undefined
