5d621587a0dbbed4e8641ba8509cab32549c12c4
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-09 06:33:23 -0800

    Add files via upload

---
+++ Comparision.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" >1);
+console.log(2 > "1");
+
+console.log(null > 0);
+console.log(null == 0);  //This type of code get confused to programmer
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0); //This type of code get confused to programmer
+console.log(undefined >= 0);
