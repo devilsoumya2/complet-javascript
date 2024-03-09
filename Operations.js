8fb71d819bb0470620a12e227e2f3537a248d74d
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-09 06:16:10 -0800

    Add files via upload

---
+++ Operations.js
@@ -0,0 +1,32 @@
+// let value=8
+// let negValue=-value
+// console.log(negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(3**3);
+
+// let str1="soumya"
+// let str2=" Ranjan"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+3)
+// console.log("1"+"3")
+// console.log("1"+3+4)
+// console.log(1+3+"4")
+
+// console.log(true)
+// console.log(+true)
+//// console.log(true+)  this is not possible in javascript
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=600
+++gameCounter
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
