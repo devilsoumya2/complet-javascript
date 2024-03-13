31a0d9ecc8bde661f083777175de97c53f0f97a0
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-13 11:16:15 -0700

    Add files via upload

---
+++ Date_Time.js
@@ -0,0 +1,21 @@
+// //Dates
+
+// let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+let myCreatedDate=new Date("03-14-2024")
+// let myCreatedDate=new Date(2024, 2 ,13)
+// let myCreatedDate=new Date(2024, 2 ,13,5,3)
+// let myCreatedDate=new Date(2024, 2 ,13)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
