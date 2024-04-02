6c6ee3c316cc9e25b85c538239cc87d54b4e0892
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-04-02 23:51:49 +0530

    Add files via upload

---
+++ Array2.js
@@ -0,0 +1,27 @@
+const heros=["allu","vijay","ram"]
+const new_heros=["superman","flash","batman"]
+
+// heros.push(new_heros)
+// console.log(heros);
+
+// const all_heros=heros.concat(new_heros)
+// console.log(all_heros);
+
+// const all_heros=[...heros, ...new_heros]
+// console.log(all_heros);   sprade operation
+
+
+
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_array=another_array.flat(Infinity)
+// console.log(real_array)    it convert into one array
+
+
+
+// console.log(Array.isArray("soumya")) 
+// console.log(Array.from("soumya"))    it convert into array
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3));
